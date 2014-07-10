f5abd50f0b5c3a5bcf57a9be294a514b80df713e
blpratt1027 <blpratt1027@gmail.com>
2014-07-09 21:41:47 -0700

    Update to Quiz

--- Quiz/js/quiz_script.js
+++ Quiz/js/quiz_script.js
@@ -1,107 +1,73 @@
 /**
  * Created by Bryan on 5/28/2014.
  *
- * TODO (for Portfolio version)
- *  - store the questions and answers in JSON files and access them using AJAX.
  */
 'use strict';
 (function() {
     $(document).ready(function(){
 
         // ---- Variables and arrays used throughout the application ----
-        var quiz_complete = false;
-        var question_count= 0;
+        var quiz_complete = false,
+            question_count= 0,
+            questions = [],
+            answers = [],
+            images = [],
+            correct_answer = [],
+            submitted_answer = [],
+            display_image = false,
+            credit_text,
+            credit_url,
+            quiz_file = 'json/js_quiz.json';    // JS / jQuery quiz is default
         // The next phase will be to store the questions and answers in separate files and import them using AJAX.
-        var questions = [
-            "What is JavaScript?",
-            "For what browser was JavaScript originally written?",
-            "What is jQuery?",
-            "Who created jQuery?",
-            "The first version of JavaScript was completed in:",
-            "jQuery is used on more than what percentage of the 10,000 most visited web sites?",
-            "jQuery eases the process of:",
-            "JavaScript was originally created by:",
-            "One of JavaScript's strongest advocates, as well as one of its harshest critics is:",
-            "jQuery's selector engine is named:"
-        ];
-        var answers = [
-            [
-                "One of the .NET family of languages.",
-                "A scripting language for running batch processes.",
-                "A sequential programming language used for applications written for the Linux operating system.",
-                "A dynamic, object oriented, event driven language used most often in web browsers."
-            ],
-            [
-                "Microsoft Internet Explorer",
-                "Mozilla Firefox",
-                "Netscape Navigator",
-                "Apple Safari"
-            ],
-            [
-                "A free, open-source JavaScript library designed to ease the development of client-side scripted HTML pages and applications.",
-                "A software platform for scalable server-side and networking applications.",
-                "A server-side scripting language designed for web development but also used as a general-purpose programming language.",
-                "A software framework that includes a large library and provides language interoperability across several programming languages."
-            ],
-            [
-                "John Resig",
-                "Tim Berners-Lee",
-                "Steve Jobs",
-                "Al Gore"
-            ],
-            [
-                "1 year",
-                "10 months",
-                "10 weeks",
-                "10 days"
-            ],
-            [
-                "30%",
-                "50%",
-                "80%",
-                "95%"
-            ],
-            [
-                "Selecting DOM elements",
-                "Creating animations",
-                "Handling events",
-                "All of the above"
-            ],
-            [
-                "Bill Gates",
-                "Brendan Eich",
-                "John Resig",
-                "Tim Berners-Lee"
-            ],
-            [
-                "Brendan Eich",
-                "Douglas Crockford",
-                "John Resig",
-                "Tim Berners-Lee"
-            ],
-            [
-                "Flash",
-                "Fuze",
-                "Sizzle",
-                "Elmer"
-            ]
-        ];
-        var images = [
-            {img_src: "images/javascript.jpg", alt: "JavaScript logo"},
-            {img_src: "images/javascript.png", alt: "JavaScript image"},
-            {img_src: "images/jquery.png", alt: "jQuery image"},
-            {img_src: "images/jquery.jpg", alt: "jQuery image"},
-            {img_src: "images/javascript_the_evil_parts.png", alt: "JavaScript image"},
-            {img_src: "images/jquery2.jpg", alt: "jQuery image"},
-            {img_src: "images/jquery2.png", alt: "jQuery image"},
-            {img_src: "images/js.jpg", alt: "JavaScript image"},
-            {img_src: "images/JS-logo1.png", alt: "JavaScript image"},
-            {img_src: "images/jquery3.png", alt: "jQuery image"},
-            {img_src: "images/done.gif", alt: "All Done!"}
-        ];
-        
-        var correct_answer = [4, 3, 1, 1, 4, 3, 4, 2, 2, 3];
-        var submitted_answer = [];
+
+        var parameter = location.search.substring(1).split('?');
+        switch (parameter[0]) {
+            case 'wf':
+                quiz_file = 'json/flowers_quiz.json';
+                break;
+            case 'us':
+                quiz_file = 'json/us_quiz.json';
+            default:
+                break;
+        }
+
+        $.getJSON(quiz_file, function(data){
+            $(".title").html(data.title);
+            for (var q = 0; q < data.questions.length; q++) {
+                questions.push(data.questions[q].q)
+            }
+
+            for (var a = 0; a < data.answers.length; a++) {
+                var temp = [];
+                for (var o = 0; o < data.answers[a].a.length; o++) {
+                    temp.push(data.answers[a].a[o].option);
+                }
+                answers.push(temp);
+            }
+            for (var i = 0; i < data.images.length; i++) {
+                temp = {"img_src": data.images[i].img_src, "alt": data.images[i].alt}
+                images.push(temp);
+            }
+
+            for (var c = 0; c < data.correct_answer.length; c++) {
+                correct_answer.push(data.correct_answer[c].a);
+            }
+
+            if (data.credit) {
+                credit_text = data.credit.text;
+                credit_url = data.credit.url;
+
+                var creditsHTML = "<a href=\"" + credit_url + "\" target=\"_blank\">Credit: " + credit_text + "</a>";
+                $("#credits").html(creditsHTML);
+            }
+
+            if (data.display_image) {display_image = true; }
+            // Execution begins here.
+            //alert("Hold it!");
+            loadQuestion();
+            $("#quiz_wrapper").fadeIn(600);
+        });
+
 
         //  -------- Event handlers --------
 
@@ -123,8 +89,10 @@
                             " questions correctly.");
                         quiz_complete = true;
                     } else {
+                        $("#quiz_wrapper").fadeOut(100);
                         // If not the last question, process...
                         processClick();
+                        $("#quiz_wrapper").fadeIn(500);
                     }
                 } else {  // If no radio button is selected, show the warning and exit the event handler
                     $('.warning').fadeIn(100);
@@ -151,14 +119,28 @@
             }
         );
 
+        // This will probably be best in the general declarations area
+        var buttonsForm = $("#buttons_form");
+        var rBtn, qSpan;
         // ------------ Functions -----------------
         var loadQuestion = function() {
             if (!quiz_complete) {
                 $('#main_image').fadeOut(200).finish();
+                buttonsForm.empty();
                 // Load questions and answers from arrays...
                 $('#current_question').text(questions[question_count]);
                 for (var i = 0; i < answers[question_count].length; i++) {
                     $('#q' + (i + 1).toFixed()).text(answers[question_count][i]);
+                    rBtn = crEl('input');
+                    rBtn.type = 'radio';
+                    rBtn.name = 'q1';
+                    rBtn.value = i + 1;
+                    qSpan = crEl('span');
+                    //qSpan.id = 'q' + (i + 1).toFixed();
+                    qSpan.innerHTML = answers[question_count][i];
+                    addToEl(buttonsForm, rBtn);
+                    addToEl(buttonsForm, qSpan);
+                    addToEl(buttonsForm, crEl('br'));
                 }
                 $("#current_question").fadeIn(400);
                 $("#main_image").attr("src", images[question_count].img_src).attr("alt", images[question_count].alt).fadeIn(800);
@@ -166,14 +148,22 @@
                 $('#quiz_question input[type=radio]').click(function () { // Create event handler for radio buttons
                     submitted_answer[question_count] = $(this).val(); // Add answer to array
                     $('.warning').fadeOut(400);
+                    $('#submit_button').removeClass('disabled');
+
                 });
             } else {
                 // Show the answers
+                // TODO - consider changing font sizes for the answer section.  It will have to be more than just the
+                // body as other elements have fixed sizes which over-ride it.
                 $("#quiz_table").remove();  // Remove the table used to house the quiz; populate a fresh page
                 $("#quiz_wrapper").append("<h2>Answers</h2>");
                 for (i = 0; i < questions.length; i++) {
                     // Load each question...
                     $("#quiz_wrapper").append("<p class='answers'><span class='ques'>Question " + (i + 1).toFixed() + "</span>: " + questions[i] + "</p>");
+                    // A small version of the image, if the display image option is flagged
+                    if (display_image) {
+                        $("#quiz_wrapper").append("<img class='tiny-img' src=\"" + images[i].img_src + "\">");
+                    }
                     // ...and the correct answer.
                     $("#quiz_wrapper").append("<p class='answers'> - <span class='ans' >Answer</span>: " + answers[i][correct_answer[i] - 1]);
                     if (correct_answer[i] == submitted_answer[i]) {
@@ -184,6 +174,7 @@
                         $("#quiz_wrapper").append("<span class='answers'>Your answer: " + answers[i][submitted_answer[i] - 1] + " <span class='incorrect'>(Incorrect)</span></span></p>");
                     }
                 }
+                $('#another_quiz').show();
             }
         }; // End loadQuestion function
 
@@ -191,6 +182,7 @@
         var processClick = function() {
             if (question_count < questions.length) {
                 loadQuestion();
+                $('#submit_button').addClass('disabled');
                 switch (question_count) {
                     case 0:
                         $("#previous_button").hide();
@@ -215,7 +207,15 @@
             return correct_count;
         }; // End countCorrect function
 
-        // Execution begins here.
-        loadQuestion();
+        var crEl = function(element) {
+            return document.createElement(element);
+        };
+
+        function addToEl(dest, element) {
+            $(dest).append(element);
+        }
+
+
+
     });
 })();
